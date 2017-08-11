bd10bf8a148634106b790a46c5f99bf3c7daa56e
beginor <beginor@qq.com>
2017-08-11 14:24:40 +0800

    01-hello,world

---
+++ 01-HelloWorld/Send/Send.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Send {
+
+    class Program {
+
+        static void Main(string[] args) {
+            var factory = new ConnectionFactory {
+                HostName = "localhost"
+            };
+            using (var connection = factory.CreateConnection()) {
+                using (var channel = connection.CreateModel()) {
+                    channel.QueueDeclare(
+                        queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+                    var message = "Hello, world!";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "hello",
+                        basicProperties: null,
+                        body: body
+                    );
+                    Console.WriteLine($" [x] Sent {message}");
+                }
+            }
+            // Console.WriteLine("Press Enter to exit!");
+            // Console.ReadLine();
+        }
+
+    }
+
+}
