93969507dd13d65bf981975a5f76b0ab657f42d3
beginor <beginor@qq.com>
2017-08-15 12:03:43 +0800

    02-work queues

---
+++ 02-WorkQueues/NewTask/NewTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace NewTask {
+
+    class Program {
+
+        static void Main(string[] args) {
+            var factory = new ConnectionFactory {
+                HostName = "localhost"
+            };
+            using (var connection = factory.CreateConnection()) {
+                using (var channel = connection.CreateModel()) {
+                    channel.QueueDeclare(
+                        queue: "task_queue",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+                    var message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "task_queue",
+                        basicProperties: null,
+                        body: body
+                    );
+                    Console.WriteLine($" [x] Sent {message}");
+                }
+            }
+            // Console.WriteLine("Press Enter to exit!");
+            // Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args) {
+            return args.Length > 0 ? string.Join(" ", args) : "Hello world!";
+        }
+    }
+}
---
+++ 02-WorkQueues/Worker/Worker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Worker {
+
+    class Program {
+
+        static void Main(string[] args) {
+            var factory = new ConnectionFactory {
+                HostName = "localhost"
+            };
+            using (var connection = factory.CreateConnection()) {
+                using (var channel = connection.CreateModel()) {
+                    channel.QueueDeclare(
+                        queue: "task_queue",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (s, e) => {
+                        var message = Encoding.UTF8.GetString(e.Body);
+                        Console.WriteLine($" [x] Received {message} .");
+                        var dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+                        Console.WriteLine(" [x] Done");
+                    };
+                    channel.BasicConsume(
+                        queue: "task_queue",
+                        autoAck: true,
+                        consumer: consumer
+                    );
+                    Console.WriteLine("Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+    }
+
+}
